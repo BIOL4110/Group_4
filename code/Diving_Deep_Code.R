876566273979f0952aad5c94bbd700e3744fc5ab
Megan Brock <brocmega@gmail.com>
2024-12-05 16:19:09 -0500

    Updating code

--- code/Diving_Deep_Code.R
+++ code/Diving_Deep_Code.R
@@ -20,8 +20,8 @@ fishmorphx <- fishmorph %>%
   rename(Species = species,
          MaxLength = MBl)
 
-A_NA_lake_datax <- A_Na_lake_data %>%
-  rename(MaxLength = `Length (cm)`)
+A_NA_lake_datax <- A_NA_lake_data %>%
+  rename(MaxLength = Length..cm.)
 
 Fish_Lake <- merge(A_NA_lake_datax, species,  by = c("Genus", "Species"))
 Fish_Lake_swim <- merge(Fish_Lake, swimming,  by = c("SpecCode"))
@@ -31,15 +31,18 @@ Fish_Length <- merge(Fish_Lake, fishmorphx,  by = c("Genus", "Species", "Family"
 ## body length (+ measurement), body shape
 Fish_selected_length <- Fish_Length %>% 
   select(Genus, Species, Lake, DepthRangeDeep, Continent, MaxLength, measurement)
+
 ## body shape
 Fish_selected_shape <- Fish_Lake %>% 
   select(Genus, Species, Lake, DepthRangeDeep, Continent, BodyShapeI)
+
 ## swim mode
 Fish_selected_swim <- Fish_Lake_swim %>% 
   select(Genus, Species, Lake, DepthRangeDeep, Continent, AdultMode)
-## egg
+
+## fertilization
 Fish_selected_repro <- Fish_Lake_repro %>% 
-  select(Genus, Species, Lake, DepthRangeDeep, Continent, Fertilization.y)
+  select(Genus, Species, Lake, DepthRangeDeep, Continent, Fertilization)
 
 Fish_filtered_length <- Fish_selected_length %>% 
   filter(DepthRangeDeep != "NA",
@@ -53,7 +56,7 @@ Fish_filtered_swim <- Fish_selected_swim %>%
          Continent == "Africa")
 Fish_filtered_repro <- Fish_selected_repro %>% 
   filter(DepthRangeDeep != "NA",
-         Fertilization.y != "NA",
+         Fertilization != "NA",
          Continent == "Africa")
 
 Fish_filtered_shape$BodyShape <- ifelse(Fish_filtered_shape$BodyShapeI == "elongated", "elongated / eel-like",
@@ -72,24 +75,26 @@ fishc <- fishb %>%
 fish_model <- fishc %>% 
   na.omit()
 
-## linear regression with boday shape
+## linear regression with body shape
 options(na.action = "na.omit")
 lm1 <- lm(DepthRangeDeep ~ MaxLength + BodyShape + AdultMode,
           data = fishc)
+
 ## linear regression with fertilization
-lm2 <- lm(DepthRangeDeep ~ MaxLength + BodyShape + Fertilization.y,
+lm2 <- lm(DepthRangeDeep ~ MaxLength + BodyShape + Fertilization,
           data = fishc)
 
 ## dredge lm1
 options(na.action = "na.fail")
 dd <- dredge(lm1)
 summary(dd)
-#dredge lm2
+
+## dredge lm2
 options(na.action = "na.fail")
 dd2 <- dredge(lm2)
 summary(dd2)
 
-#find largest AICc
+## find largest AICc
 dd_large <- which.max(dd$AICc) 
 
 dd[dd_large, ]
@@ -98,21 +103,13 @@ dd[order(dd$AICc, decreasing = TRUE), ]
 
 model.avg(dd, subset = delta < 4)
 
-#running models
+## running models
 depth_lenght_lm <- lm(DepthRangeDeep ~ MaxLength ,data = fishc)
 summary(depth_lenght_lm)
 
 depth_lenght_shape_lm <- lm(DepthRangeDeep ~ MaxLength + BodyShape ,data = fishc)
 summary(depth_lenght_shape_lm)
 
-#Visreg plots
-
+## visreg plots
 visreg1 <- visreg(lm1)
 visreg2 <- visreg(lm2)
-
-
-
-
-
-
-
